9b5c0870006c4b85cff615b7668c7397419aa142
ShivamSali <shivamsali81097@gmail.com>
2022-07-30 18:48:31 +0530

    first commit

--- apiDocument.md
+++ apiDocument.md
@@ -1,10 +1,47 @@
 //Page 1
 
-# categories Link
-http://localhost:7800/categories
+# To see the Mens collection
 
-# sub_categories Link
-http://localhost:7800/sub_categories
+https://localhost:7800/products/1
 
-# products Link
-http://localhost:7800/products
+# To see the Womens collection
+
+https://localhost:7800/products/2
+
+# To see the kids collection
+
+https://localhost:7800/products/3
+
+# To see the Accessories collection
+
+https://localhost:7800/products/4
+
+
+// Page 2
+
+# To see T-shirts product wrt Men
+https://localhost:7800/products/1?subcategoryId=0
+# To see Shirts product wrt Men
+https://localhost:7800/products/1?subcategoryId=1
+# To see Trousers product wrt Men
+https://localhost:7800/products/1?subcategoryId=2
+# To see Casuals product wrt Men
+https://localhost:7800/products/1?subcategoryId=3
+# To see Sports product wrt Men
+https://localhost:7800/products/1?subcategoryId=4
+# To see Sweatshirts product wrt Men
+https://localhost:7800/products/1?subcategoryId=5
+# To see Jeans product wrt Men
+https://localhost:7800/products/1?subcategoryId=6
+# To see Shoes product wrt Men
+https://localhost:7800/products/1?subcategoryId=7
+# To see Formals product wrt Men
+https://localhost:7800/products/1?subcategoryId=8
+# To see Watches product wrt Men
+https://localhost:7800/products/1?subcategoryId=9
+
+//same as for women and kids just have to change
+
+//for women  7800/products/2?subcategoryId=
+
+//for kids  7800/products/3?subcategoryId=
--- app.js
+++ app.js
@@ -1,45 +1,81 @@
 let express = require('express');
 let app = express();
-
+let morgan = require('morgan');
 let dotenv = require('dotenv');
 dotenv.config();
-
 let port = process.env.PORT || 9870;
 let mongo = require('mongodb');
-let MongoClient = mongo.MongoClient;
-let mongoUrl = "mongodb+srv://Shivam:Shivam123@cluster0.qx3cftb.mongodb.net/?retryWrites=true&w=majority";
+let MongoClient = mongo.MongoClient
+let mongoUrl = "mongodb+srv://Shivam:Shivam123@cluster0.qx3cftb.mongodb.net/?retryWrites=true&w=majority"
 let db;
 
-app.get('/',(req,res)=>{
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
     res.send('Hii From Express')
 })
 
-app.get('/categories',(req,res)=>{
-    db.collection('categories').find().toArray((err,result) => {
-        if(err) throw err;
-        res.send(result)
-    })
-}) 
-
-app.get('/sub_categories',(req,res) =>{
-    db.collection('sub_categories').find().toArray((err,result) => {
-        if(err) throw err;
+app.get('/categories', (req, res) => {
+    db.collection('categories').find().toArray((err, result) => {
+        if (err) throw err;
         res.send(result)
     })
 })
 
-app.get('/products',(req,res) =>{
-    db.collection('products').find().toArray((err,result) => {
-        if(err) throw err;
+app.get('/sub_categories', (req, res) => {
+    db.collection('sub_categories').find().toArray((err, result) => {
+        if (err) throw err;
         res.send(result)
     })
 })
+
+app.get('/products', (req, res) => {
+    db.collection('products').find().toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+})
+
+// app.get('/products/:id', (req, res) => {
+//     let id = req.params.id;
+//     db.collection('products').find({ subCategory_id: Number(id) }).toArray((err, result) => {
+//         if (err) throw err;
+//         res.send(result)
+//     })
+// })
+
+
+
+app.get('/products/:categoryId', (req, res) => {
+    let query = {}
+    let categoryId = Number(req.params.categoryId);
+    let subcategoryId = Number(req.query.subcategoryId);
+    if (subcategoryId) {
+        query = {
+            // subcategory_id: subcategoryId,
+            // "color.color_id": colorId
+            category_id:categoryId,
+            "sub-category_id":subcategoryId
+        }
+    }
+    else {
+        query = {
+            category_id:categoryId
+        }
+    }
+
+    db.collection('products').find(query).toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+})
+
 
 // connection with db
-MongoClient.connect(mongoUrl,(err,client)=>{
-    if(err) console.log(`Error While Connecting`);
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) console.log(`Error While Connecting`);
     db = client.db('edurekaEcomm')
-    app.listen(port,() => {
+    app.listen(port, () => {
         console.log(`listening on port ${port}`)
     })
 })
